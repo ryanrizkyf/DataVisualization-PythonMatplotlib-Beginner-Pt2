f7c15ab358b55b5f6030cdfd00a294c2101ccd18
Ryan <ryanrizkyf@gmail.com>
2021-02-19 10:16:14 +0700

    edit commit

---
+++ Membuat_Histogram_Scatterplot/Membuat_Histogram_Pt_Dua.py
@@ -0,0 +1,39 @@
+# dataset = https://dqlab-dataset.s3-ap-southeast-1.amazonaws.com/retail_raw_reduced.csv
+
+# Import library
+import datetime
+import pandas as pd
+import matplotlib.pyplot as plt
+# Baca dataset
+dataset = pd.read_csv(
+    'retail_raw_reduced.csv')
+# Buat kolom baru yang bertipe datetime dalam format '%Y-%m'
+dataset['order_month'] = dataset['order_date'].apply(
+    lambda x: datetime.datetime.strptime(x, "%Y-%m-%d").strftime('%Y-%m'))
+# Buat Kolom GMV
+dataset['gmv'] = dataset['item_price']*dataset['quantity']
+
+# Berhubung kita mau fokus ke provinsi DKI Jakarta dan bulan Oktober-Desember 2019,
+# kita filter saja datanya dan disimpan ke dataframe baru
+dataset_dki_q4 = dataset[(dataset['province'] == 'DKI Jakarta') & (
+    dataset['order_month'] >= '2019-10')]
+
+# buat dataframe baru bernama data_per_customer_dki_q4.
+data_per_customer = (dataset_dki_q4.groupby('customer_id')
+                                   .agg({'order_id': 'nunique',
+                                         'quantity': 'sum',
+                                         'gmv': 'sum'})
+                                   .reset_index()
+                                   .rename(columns={'order_id': 'orders'}))
+print(data_per_customer.sort_values(by='orders', ascending=False))
+
+# membuat histogram untuk quantity
+plt.figure(figsize=(10, 5))
+plt.hist(data_per_customer['quantity'],
+         bins=100, range=(1, 200), color='brown')
+plt.title('Distribution of Total Quantity per Customer\nDKI Jakarta in Q4 2019',
+          fontsize=15, color='blue')
+plt.xlabel('Quantity', fontsize=12)
+plt.ylabel('Number of Customers', fontsize=12)
+plt.xlim(xmin=0, xmax=200)
+plt.show()
